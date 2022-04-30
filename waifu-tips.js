fb90c2e95f9db25d874665387ee862583aefb1fb
narutozxp <41829090+narutozxp@users.noreply.github.com>
2022-04-30 15:33:38 +0800

    24

--- waifu-tips.js
+++ waifu-tips.js
@@ -163,7 +163,7 @@ function loadWidget(config) {
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			modelId = 0; // 模型 ID
-			modelTexturesId = 35; // 材质 ID
+			modelTexturesId = 24; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
